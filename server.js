84cd1b149132dbd0a850c73117be09caa8ef647b
EvgeniyGrokholskiy <grokholskiy@gmail.com>
2023-09-12 12:07:25 +0500

    fix_(server.js): fix import order error_12.09.23.

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-import fallback from 'express-history-api-fallback';
 import express from 'express';
 import path from 'path';
+import fallback from 'express-history-api-fallback';
 import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
@@ -8,7 +8,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
-const root = `${__dirname}/dist`
+const root = `${__dirname}/dist`;
 const PORT = 3000;
 
 app.use(express.static(root));
