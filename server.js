4b17121da00f0bbc8799eb71c9b17fba0a39084a
EvgeniyGrokholskiy <grokholskiy@gmail.com>
2023-09-12 12:01:07 +0500

    fix_(server.js): fix Cannot GET /sign-up_12.09.23.

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
+import fallback from 'express-history-api-fallback';
 import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+
+const __dirname = path.dirname(__filename);
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const root = `${__dirname}/dist`
+const PORT = 3000;
 
-app.use(express.static('./dist'));
+app.use(express.static(root));
+app.use(fallback('index.html', { root }));
 
 app.listen(PORT, () => {
   // console.log(`Example app listening on port ${PORT}!`);
