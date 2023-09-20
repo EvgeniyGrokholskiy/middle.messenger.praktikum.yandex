95daa30431eb376db3adc48af6fd18c68f374c6e
EvgeniyGrokholskiy <grokholskiy@gmail.com>
2023-09-21 00:13:22 +0500

    refact_(server): some refactoring_21.09.23.

---
+++ mochaSetup.js
@@ -0,0 +1,17 @@
+import { JSDOM } from "jsdom";
+import fs from "fs";
+import Handlebars from "handlebars";
+
+const { window } = new JSDOM('<div id="app"></div>', {
+  url: "http://localhost:3000",
+});
+
+global.window = window;
+global.document = window.document;
+global.DocumentFragment = window.DocumentFragment;
+
+require.extensions[".hbs"] = function (module, filename) {
+  const contents = fs.readFileSync(filename, "utf-8");
+
+  module.exports = Handlebars.compile(contents);
+};
